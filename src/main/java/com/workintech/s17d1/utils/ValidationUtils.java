b8cbed314f21eebb1b0f4353b119b2774e205232
samet7ozturk <sametozturk973@gmail.com>
2024-02-07 21:03:28 +0300

    Complete

---
+++ src/main/java/com/workintech/s17d1/utils/ValidationUtils.java
@@ -0,0 +1,20 @@
+package com.workintech.s17d1.utils;
+
+import com.workintech.s17d1.entity.Animal;
+
+public class ValidationUtils {
+    public static void checkId(Integer id){
+        if(id==null || id<0){
+            System.out.println("id cannot be null or less than zero!");
+        }
+    }
+
+    public static void checkAnimal(Animal animal){
+        if(animal.getId()<0){
+            System.out.println("id cannot be less than zero!");
+        }
+        if(animal.getName()==null ||animal.getName().isEmpty()){
+            System.out.println("name can not be null or empty!");
+        }
+    }
+}
---
+++ src/test/java/com/workintech/s17d1/S17d1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workintech.s17d1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S17d1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
